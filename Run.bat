7475e9eec237859da16252b299bab5ae252f4647
Crimson17 <46162167+Crimson17@users.noreply.github.com>
2021-07-23 19:56:47 +0200

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,4 +1,4 @@
 @echo off
 cls
 for %%I in (.) do set filename=%%~nxI
-g++ -I./include ./src/*.c ./src/*.cpp -o./out/%filename%.exe -L./lib -lsfml-graphics-s -lsfml-window-s -lsfml-audio-s -lsfml-network-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32 -lopenal32 -lflac -lvorbisenc -lvorbisfile -lvorbis -logg -lws2_32 && start ./out/%filename%.exe
+g++ -I./include ./src/*.cpp -o./out/%filename%.exe -L./lib -lsfml-graphics-s -lsfml-window-s -lsfml-audio-s -lsfml-network-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32 -lopenal32 -lflac -lvorbisenc -lvorbisfile -lvorbis -logg -lws2_32 && start ./out/%filename%.exe
